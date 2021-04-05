66789bfd82a6467f83f321e7cae43061d3e477c0
Fellipe Henry Bordon <68597496+Fhb-Hub@users.noreply.github.com>
2021-04-05 20:11:35 -0300

    Update validator_helper.dart

--- lib/helpers/validator_helper.dart
+++ lib/helpers/validator_helper.dart
@@ -3,11 +3,14 @@ const kRequiredBeoforePoint = 'Digite um número antes do caracter ponto (.)';
 const kRequiredAfterPoint = 'Digite um número depois do caracter ponto (.)';
 const kOnlyOnePoint = 'O campo não pode conter mais de um ponto.';
 const kNotNull = 'O campo não pode ser nulo.';
+const kOnlyPositive = 'O campo não pode ser negativo';
 
 class ValidatorHelper {
   static String condition(String text) {
     if (text.isEmpty) {
       return kRequiredField;
+    } else if (text.contains('-')) {
+      return kOnlyPositive;
     } else if (text.length - text.replaceAll('.', '').length > 1) {
       return kOnlyOnePoint;
     } else if (text.contains('.')) {
